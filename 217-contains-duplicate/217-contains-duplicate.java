a7d95eba12901664973bd70738de0d14eebe3883
Sayed Khaled <63265214+SayedKhaledd@users.noreply.github.com>
2022-07-26 01:09:23 +0200

    Time: 12 ms (61.82%), Space: 68.8 MB (59.45%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -2,22 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-                Hashtable<Integer, Integer> table = new Hashtable<>();
-
-
-        for (int i = 0; i < nums.length; i++) {
-                 int x=nums[i];
-            if (table.get(x) != null)
-                table.put(x, table.get(x) + 1);
-            else
-                table.put(x, 1);
+  
+        Set < Integer > st = new HashSet < > ();
+        for (int i: nums) {
+            if (st.contains(i)) return true;
+            st.add(i);
         }
-        for (int i = 0; i < table.size(); i++) {
-            if (table.get(nums[i]) > 1) {
-return true;           
-            }
-
-        }
-        return false;       
+        return false;
+    
     }
 }
