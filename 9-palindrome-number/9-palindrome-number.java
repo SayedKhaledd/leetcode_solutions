a7b767b945023212b81c8fc9329ebe8c7167ddc0
Sayed Khaled <63265214+SayedKhaledd@users.noreply.github.com>
2022-07-27 03:38:53 +0200

    Time: 14 ms (60.41%), Space: 44.9 MB (40.35%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,10 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s=x+"";
+/*        String s=x+"";
         for(int i=0,j=s.length()-1;i<s.length();i++,j--){
             if(s.charAt(i)!=s.charAt(j))
                 return false;
         }
         return true;
-    }
+  */
+        if(x<0 || (x%10==0 && x!=0))
+            return false;
+        int y=0;
+        while(x>y){
+            y=y*10+x%10;
+            x/=10;
+            
+        }
+        return x==y || x==y/10;
+  }
 }
