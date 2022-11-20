220fed04672bf82278e5ca149f179c98c19d579d
Sayed Khaled <63265214+SayedKhaledd@users.noreply.github.com>
2022-11-20 23:53:50 +0200

    Time: 0 ms (100.00%), Space: 54.4 MB (35.27%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+           int high = nums.length-1, low = 0;
+        int index = -1;
+        while (low<=high) {
+   int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                break;
+            } else if (nums[mid] > target) {
+                high = mid-1;
+            } else
+                low = mid+1;
+        }
+        return index;
+    }
+}
